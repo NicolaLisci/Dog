2a7da3c776587935af5582b59537ad3f397ff7b8
MatthewPili <39949772+MatthewPili@users.noreply.github.com>
2018-06-06 10:25:51 +0200

    Classi cane-dogfriendly-razza

---
+++ src/it/ats/modello/DogFriendly.java
@@ -0,0 +1,49 @@
+package it.ats.modello;
+
+public class DogFriendly {
+
+	private int id;
+	private int idAttivita;
+	private String nomeAttivita;
+
+	public DogFriendly(int id, String nomeAttivita, int idAttivita) {
+		super();
+		this.id = id;
+		this.nomeAttivita = nomeAttivita;
+		this.idAttivita = idAttivita;
+	}
+
+	public DogFriendly() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNomeAttivita() {
+		return nomeAttivita;
+	}
+
+	public void setNomeAttivita(String nomeAttivita) {
+		this.nomeAttivita = nomeAttivita;
+	}
+
+	public int getIdAttivita() {
+		return idAttivita;
+	}
+
+	public void setIdAttivita(int idAttivita) {
+		this.idAttivita = idAttivita;
+	}
+
+	@Override
+	public String toString() {
+		return "DogFriendly [id=" + id + ", nomeAttivita=" + nomeAttivita + ", idAttivita=" + idAttivita + "]";
+	}
+
+}
