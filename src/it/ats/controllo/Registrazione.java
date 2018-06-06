9fb0457ce9919b2b83694af5b20f79abb67e4dfc
NicolaLisci <liscinicola@live.it>
2018-06-06 20:55:30 +0200

    4

--- src/it/ats/controllo/Registrazione.java
+++ src/it/ats/controllo/Registrazione.java
@@ -40,77 +40,77 @@ public class Registrazione extends HttpServlet {
 
 		Integer matricola = null;
 
-		if (stringMatricola == null || "".equals(stringMatricola)) {
+//		if (stringMatricola == null || "".equals(stringMatricola)) {
+//
+//			errors.put("error_matricola", "Campo matricola non può essere vuoto");
+//
+//		} else {
+//
+//			try {
+//
+//				matricola = Integer.parseInt(stringMatricola);
+//
+//			} catch (NumberFormatException nfe) {
+//
+//				errors.put("error_matricola", "Campo matricola deve essere un numero");
+//
+//			}
+//
+//		}
+//
+//		if (nome == null || "".equals(nome)) {
+//
+//			errors.put("error_nome", "Campo nome non può essere vuoto");
+//
+//		}
+//
+//		if (cognome == null || "".equals(cognome)) {
+//
+//			errors.put("error_cognome", "Campo cognome non può essere vuoto");
+//
+//		}
+//
+//		if (errors.isEmpty()) {
+//
+//			Studente studente = new Studente();
+//
+//			studente.setMatricola(matricola);
+//
+//			studente.setNome(nome);
+//
+//			studente.setCognome(cognome);
+//
+//			DAOStudente daoStudente = new DAOStudenteImpl();
+//
+//			try {
+//
+//				daoStudente.save(studente);
+//
+//				RequestDispatcher requestDispatcher = request.getRequestDispatcher("registrazioneSuccesso.jsp");
+//
+//				requestDispatcher.forward(request, response);
+//
+//			} catch (DAOException e) {
+//
+//				System.out.println(e.getMessage());
+//
+//				response.sendRedirect("error.jsp");
+//
+//			}
+//
+//		} else {
+//
+//			for (Entry<String, String> entry : errors.entrySet()) {
+//
+//				request.setAttribute(entry.getKey(), entry.getValue());
+//
+//			}
+//
+//			RequestDispatcher requestDispatcher = request.getRequestDispatcher("registrazione.jsp");
+//
+//			requestDispatcher.forward(request, response);
 
-			errors.put("error_matricola", "Campo matricola non può essere vuoto");
-
-		} else {
-
-			try {
-
-				matricola = Integer.parseInt(stringMatricola);
-
-			} catch (NumberFormatException nfe) {
-
-				errors.put("error_matricola", "Campo matricola deve essere un numero");
-
-			}
-
-		}
-
-		if (nome == null || "".equals(nome)) {
-
-			errors.put("error_nome", "Campo nome non può essere vuoto");
-
-		}
-
-		if (cognome == null || "".equals(cognome)) {
-
-			errors.put("error_cognome", "Campo cognome non può essere vuoto");
-
-		}
-
-		if (errors.isEmpty()) {
-
-			Studente studente = new Studente();
-
-			studente.setMatricola(matricola);
-
-			studente.setNome(nome);
-
-			studente.setCognome(cognome);
-
-			DAOStudente daoStudente = new DAOStudenteImpl();
-
-			try {
-
-				daoStudente.save(studente);
-
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("registrazioneSuccesso.jsp");
-
-				requestDispatcher.forward(request, response);
-
-			} catch (DAOException e) {
-
-				System.out.println(e.getMessage());
-
-				response.sendRedirect("error.jsp");
-
-			}
-
-		} else {
-
-			for (Entry<String, String> entry : errors.entrySet()) {
-
-				request.setAttribute(entry.getKey(), entry.getValue());
-
-			}
-
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("registrazione.jsp");
-
-			requestDispatcher.forward(request, response);
-
-		}
+		//}
 
 	}
 
