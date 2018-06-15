1c8457b782fd807f9077c142edd9095e44285a87
MatthewPili <39949772+MatthewPili@users.noreply.github.com>
2018-06-15 15:57:54 +0200

    dacane aggiornato

--- src/it/ats/persistenza/DAOCane.java
+++ src/it/ats/persistenza/DAOCane.java
@@ -1,5 +1,7 @@
 package it.ats.persistenza;
 
+import java.util.List;
+
 import it.ats.modello.Cane;
 
 public interface DAOCane {
@@ -7,7 +9,7 @@ void salva(Cane cane) throws DAOException;
 void aggiorna(Cane cane) throws DAOException;
 void elimina(int idCane) throws DAOException;
 Cane vediTutto( int id_cane) throws DAOException;
-	
+List<Cane> elencoCani(int idUtente) throws DAOException;	
 
 
 }
